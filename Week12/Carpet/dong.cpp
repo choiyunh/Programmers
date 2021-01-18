56b400a6bee13dfc75eb2a874eb810c361ae9fc6
dongyeon-0822 <myjjue00@gmail.com>
2021-01-18 17:57:21 +0900

    Update dong.cpp

--- Week12/Carpet/dong.cpp
+++ Week12/Carpet/dong.cpp
@@ -3,15 +3,15 @@
 #include <cmath>
 using namespace std;
 
-void solve(double a, double b, double c,int &x,int &y) {
+void solve(int a, int b, int c,int &x,int &y) {
     x = (-b - sqrt(b * b - 4 * a * c)) / 2 * a;
     y = (-b + sqrt(b * b - 4 * a * c)) / 2 * a;
 }
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int x, y;
-    solve(1, (brown + 4) / 2, brown + yellow, x, y);
-    answer.push_back(-x);
-    answer.push_back(-y);
+    solve(1, (brown + 4) / -2, brown + yellow, x, y);
+    answer.push_back(y);
+    answer.push_back(x);
     return answer;
 }
