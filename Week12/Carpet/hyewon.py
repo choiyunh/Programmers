21e46cdc2de9abee58e4ba03001ab763d149012e
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-01-18 17:27:17 +0900

    21/01/18 solved carpet - hyewon

---
+++ Week12/Carpet/hyewon.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+def solution(brown, yellow):
+    answer = []
+    out = 0
+    total = brown + yellow
+    for i in range(1, total+1):
+        if total % i != 0:
+            continue
+        for j in range(1, i+1):
+            if i * j == total:
+                if (4 + ((i - 2) + (j - 2)) * 2) == brown:
+                    answer.append(i)
+                    answer.append(j)
+                    out = 1
+        if out == 1:
+            break
+
+
+    return answer
+
+print(solution(10, 2))
+print(solution(8, 1))
+print(solution(24, 24))
